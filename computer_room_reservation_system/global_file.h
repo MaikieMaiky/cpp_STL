ca3e37a359fac70c33c30c48118fe5ebb62e3a0a
Maikie <1070606690@qq.com>
2026-06-02 20:25:15 +0800

    feat: 添加全局文件定义，包含学生、教师、管理员和机器房预约相关的文件路径常量

---
+++ computer_room_reservation_system/global_file.h
@@ -0,0 +1,11 @@
+#ifndef __GLOBAL_FILE
+#define __GLOBAL_FILE
+
+
+const char kStudentFile[] = "data/student.csv";
+const char kTeacherFile[] = "data/teacher.csv";
+const char kAdminFile[] = "data/admin.csv";
+const char kMachineRoomFile[] = "data/machine_room.csv";
+const char kReservationFile[] = "data/reservation.csv";
+
+#endif  // __GLOBAL_FILE
