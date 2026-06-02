d575483cf1ef08487f03cf21badaa0c69b698511
Maikie <1070606690@qq.com>
2026-06-02 20:37:57 +0800

    feat: 更新全局文件定义为使用string类型，并在主程序中添加登录功能实现

--- computer_room_reservation_system/global_file.h
+++ computer_room_reservation_system/global_file.h
@@ -1,11 +1,18 @@
 #ifndef __GLOBAL_FILE
 #define __GLOBAL_FILE
 
+#include <string>
+using namespace std;
 
-const char kStudentFile[] = "data/student.csv";
-const char kTeacherFile[] = "data/teacher.csv";
-const char kAdminFile[] = "data/admin.csv";
-const char kMachineRoomFile[] = "data/machine_room.csv";
-const char kReservationFile[] = "data/reservation.csv";
+// 学生文件
+const string kStudentFile = "data/student.csv";
+// 教师文件
+const string kTeacherFile = "data/teacher.csv";
+// 管理员文件
+const string kAdminFile = "data/admin.csv";
+// 机房文件
+const string kMachineRoomFile = "data/machine_room.csv";
+// 预约文件
+const string kReservationFile = "data/reservation.csv";
 
 #endif  // __GLOBAL_FILE
--- computer_room_reservation_system/main.cpp
+++ computer_room_reservation_system/main.cpp
@@ -1,6 +1,63 @@
 #include "computer_room_reservation_system.h"
 #include <iostream>
+#include <fstream>
+#include "global_file.h"
 using namespace std;
+
+// login global function
+// 参数1: 文件名 参数2：操作身份类型
+void Login(string filename, int type)
+{
+    ifstream ifs;
+    ifs.open(filename, ios::in);
+    // 文件打开失败
+    if (!ifs.is_open())
+    {
+        cout << "File open error" << endl;
+        return;
+    }
+
+    // 文件打开成功
+    // 接收用户信息
+    int id = 0;
+    string name;
+    string password;
+    switch (type)
+    {
+        case 1:
+            // 学生登录
+            cout << "Please input your student id: ";
+            cin >> id;
+            break;
+        case 2:
+            // 教师登录
+            cout << "Please input your teacher id: ";
+            cin >> id;
+            break;
+    }
+    // 输入用户名和密码
+    cout << "Please input your name: ";
+    cin >> name;
+    cout << "Please input your password: ";
+    cin >> password;
+
+    // 身份验证
+    switch (type)
+    {
+        case 1:
+            // 学生身份验证
+            break;
+        case 2:
+            // 教师身份验证
+            break;
+        case 3:
+            // 管理员身份验证
+            break;
+    }
+
+    ifs.close();
+}
+
 
 int main()
 {
@@ -16,14 +73,17 @@ int main()
             case 1:
                 // 学生登录
                 cout << "Student Login" << endl;
+                Login(kStudentFile, 1);
                 break;
             case 2:
                 // 教师登录
                 cout << "Teacher Login" << endl;
+                Login(kTeacherFile, 2);
                 break;
             case 3:
                 // 管理员登录
                 cout << "Admin Login" << endl;
+                Login(kAdminFile, 3);
                 break;
             case 4:
                 // 退出系统
