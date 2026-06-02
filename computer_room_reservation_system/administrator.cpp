214b0793aa3f15e649d93ee6fede19e70737d266
Maikie <1070606690@qq.com>
2026-06-03 00:49:31 +0800

    feat: 完善管理员功能模块，添加账号管理功能 - 添加账号功能，支持教师和学生账号的创建

--- computer_room_reservation_system/administrator.cpp
+++ computer_room_reservation_system/administrator.cpp
@@ -1,5 +1,7 @@
 #include "administrator.h"
 #include <iostream>
+#include "global_file.h"
+#include <fstream>
 using namespace std;
 
 // 显示子菜单
@@ -18,10 +20,61 @@ void Administrator::ShowMenu()
 }
 
 
-// 添加账号
+// 添加账号 先不考虑重复账号 保留注释以便后续实现
 void Administrator::AddAccount()
 {
-    
+    // 1.choose the type of account: Teacher or Student
+    cout << "Please input the type of account: " << endl;
+    cout << "1. Teacher" << endl;
+    cout << "2. Student" << endl;
+
+
+    // 2.input the account information
+    string filename = "";
+    string tip = "";
+    int id = 0;
+    string name;
+    string password;
+    int type = 0;
+    cin >> type;
+    if (type == 1)
+    {
+        // 添加教师账号
+        filename = kTeacherFile;
+        tip = "Please input the teacher id: ";
+    }
+    else if (type == 2)
+    {
+        // 添加学生账号
+        filename = kStudentFile;
+        tip = "Please input the student id: ";
+    }
+    else
+    {
+        cout << "Invalid input, please try again" << endl;
+        return;
+    }
+    // 输入账号信息
+    cout << tip;
+    cin >> id;
+    cout << "Please input the name: ";
+    cin >> name;
+    cout << "Please input the password: ";
+    cin >> password;
+
+    // 3.save the account information to the file
+    // 打开文件 append
+    ofstream ofs;
+    ofs.open(filename, ios::app);
+    if (!ofs.is_open())
+    {
+        cout << "File open error" << endl;
+        return;
+    }
+    ofs << id << "," << name << "," << password << endl;
+    ofs.close();
+    cout << "Add account successfully" << endl;
+    return;
 }
 // 查看账号
 void Administrator::ViewAccount()
